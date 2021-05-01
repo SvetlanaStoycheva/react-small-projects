1711a68e71e8ef733802a419447a0ef6c4e909ea
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-05-01 18:36:07 +0300

    eighth commit

--- 23-quiz/src/context.js
+++ 23-quiz/src/context.js
@@ -5,16 +5,114 @@ const table = {
   sports: 21,
   history: 23,
   politics: 24,
+  music: 12,
 }
 
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
 const url = ''
+const tempUrl =
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [waiting, setWaiting] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [error, setError] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  //setup form
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: 'sports',
+    difficulty: 'easy',
+  })
+
+  const fetchQuestions = async (url) => {
+    setLoading(true)
+    setWaiting(false)
+    const response = await axios(url).catch((err) => console.log(err))
+    // console.log(response)
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setQuestions(data)
+        setError(false)
+        setLoading(false)
+        setWaiting(false)
+      }
+    } else {
+      setWaiting(true)
+      setError(true)
+    }
+  }
+
+  const nextQuestion = () => {
+    if (index < questions.length - 1) {
+      setIndex(index + 1)
+    } else {
+      openModal()
+    }
+  }
+
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((oldState) => oldState + 1)
+    }
+    nextQuestion()
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setWaiting(true)
+    setCorrect(0)
+  }
+
+  // useEffect(() => {
+  //   fetchQuestions(tempUrl)
+  // }, [])
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setQuiz({ ...quiz, [name]: value })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchQuestions(
+      `${API_ENDPOINT}amount=${quiz.amount}&category=${
+        table[quiz.category]
+      }&difficulty=${quiz.difficulty}&type=multiple`
+    )
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+        nextQuestion,
+        checkAnswer,
+        closeModal,
+        quiz,
+        handleSubmit,
+        handleChange,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
 // make sure use
 export const useGlobalContext = () => {
--- README.md
+++ README.md
@@ -162,3 +162,11 @@
 - remove and show more buttons
 - server side pagination. Prev/next button, when we click them, we fetch a new page from the server.
 - useReducer and Context Api
+
+23. Setup Quiz
+
+- setup quiz form where user can set the number of questions, category and difficulty and generate a quiz
+- Api: https://opentdb.com
+- library: axios, npm install axios; Axios is designed to handle http requests and responses.
+  It's used more often than Fetch because it has a larger set of features and it supports older browsers.
+- control input setup form
